fdd7a0de1f96434a70e1da698ff7625d00a13e2a
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-03 11:45:45 -0600

    complementos del workbox para un service worker y  manifiesto

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+//const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Agregar y configurar complementos del workbox para un service worker y un archivo de manifiesto.
 // TODO: Agregar cargadores de CSS y babel al webpack.
@@ -18,6 +19,27 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+                new HtmlWebpackPlugin({
+                  template: './index.html',
+                  title: 'JATE'
+                }),
+                new GenerateSW(),
+                new WebpackPwaManifest({
+                  name: 'JATE',
+                  short_name: 'JATE',
+                  description: 'solo otro editor de texto!',
+                  background_color: '#7eb4e2',
+                  theme_color: '#7eb4e2',
+                  start_url: './',
+                  publicPath: './',
+                  icons: [
+                    {
+                      src: path.resolve('src/images/logo.png'),
+                      sizes: [96, 128, 192, 256, 384, 512],
+                      destination: path.join('assets', 'icons'),
+                    },
+                  ],
+                }),                
       
     ],
 
