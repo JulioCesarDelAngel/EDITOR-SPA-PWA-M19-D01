c9575832b9df28c0119b0681ac1eb521ec740be9
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-03 19:42:52 -0600

    se implementa insertar datos a indexDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,16 @@ const initdb = async () =>
   });
 
 // TODO: Agregar lógica a un método que acepte algo de contenido y lo agregue a la base de datos
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('putDb  implemented');
+  const db = await openDB('jate',1);
+  const tr = db.transaction('jate','readwrite');
+  const store = tr.objectStore('jate');
+  const request = store.add({jdata: content});
+  const result = await request;
+  console.log('Insert:', result);
+
+};
 
 // TODO: Agregar lógica para un método que obtiene todo el contenido de la base de datos
 export const getDb = async () => console.error('getDb not implemented');
