5e8725db447624543bc0b3390ad84c7b2a82fae8
Julio C. MW11 <sysa.mail@gmail.com>
2023-02-07 23:34:59 -0600

    se agrega getDb, se corrige putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,17 +14,28 @@ const initdb = async () =>
 
 // TODO: Agregar lógica a un método que acepte algo de contenido y lo agregue a la base de datos
 export const putDb = async (content) => {
-  console.log('putDb  implemented');
   const db = await openDB('jate',1);
   const tr = db.transaction('jate','readwrite');
   const store = tr.objectStore('jate');
-  const request = store.add({jdata: content});
+  const request = store.put({id:1, jdata: content});
   const result = await request;
-  console.log('Insert:', result);
-
 };
 
 // TODO: Agregar lógica para un método que obtiene todo el contenido de la base de datos
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  //console.error('getDb SI implemented') 
+  const db = await openDB('jate',1);
+  const tr = db.transaction('jate','readonly');
+  const store = tr.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  let data = ``;
+  for (let row of result){
+    data += `${row.jdata}` ;
+  }
+  return data;  
+};
+
+
 
 initdb();
